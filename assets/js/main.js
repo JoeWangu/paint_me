4c06fb7c9fa9295e3a55ca0a9600902a6ef84534
JoeWangu <joewangu2021@outlook.com>
2024-08-09 01:23:36 +0300

    add images

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,3 +80,84 @@ backToTopButton.addEventListener('click', (event) => {
     event.preventDefault();
     scrollToTop();
 });
+
+
+// ---------------------------------------------------
+// --------------------- GLIGHTBOX  ----------------------
+// ---------------------------------------------------
+/**
+ * Initiate glightbox
+ */
+const glightbox = GLightbox({
+    selector: '.glightbox'
+});
+
+/**
+ * Init isotope layout and filters
+ */
+document.querySelectorAll('.isotope-layout').forEach(function (isotopeItem) {
+    let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+    let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+    let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+
+    let initIsotope;
+    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function () {
+        initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+            itemSelector: '.isotope-item',
+            layoutMode: layout,
+            filter: filter,
+            sortBy: sort
+        });
+    });
+
+    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function (filters) {
+        filters.addEventListener('click', function () {
+            isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+            this.classList.add('filter-active');
+            initIsotope.arrange({
+                filter: this.getAttribute('data-filter')
+            });
+            if (typeof aosInit === 'function') {
+                aosInit();
+            }
+        }, false);
+    });
+
+});
+
+/**
+ * Init swiper sliders
+ */
+function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
+        let config = JSON.parse(
+            swiperElement.querySelector(".swiper-config").innerHTML.trim()
+        );
+
+        if (swiperElement.classList.contains("swiper-tab")) {
+            initSwiperWithCustomPagination(swiperElement, config);
+        } else {
+            new Swiper(swiperElement, config);
+        }
+    });
+}
+
+window.addEventListener("load", initSwiper);
+
+/**
+ * Correct scrolling position upon page load for URLs containing hash links.
+ */
+window.addEventListener('load', function (e) {
+    if (window.location.hash) {
+        if (document.querySelector(window.location.hash)) {
+            setTimeout(() => {
+                let section = document.querySelector(window.location.hash);
+                let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+                window.scrollTo({
+                    top: section.offsetTop - parseInt(scrollMarginTop),
+                    behavior: 'smooth'
+                });
+            }, 100);
+        }
+    }
+});
